fe50651282df7d5c0283a2cac97656242fc31c9d
Rifqi Charisma <rifqibarker@gmail.com>
2022-01-13 12:37:07 +0700

    hapus kode

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("click",function (e) {
-    if(e.target.classList.contains("gallery-item")) {
-        const src = e.target.getAttribute("src");
-        document.querySelector(".modal-img").src = src;
-        var myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
-        myModal.show();
-    }
-})
